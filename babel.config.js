13c7f1cd6bc276898e77f6fc38a1a69c7bc119e7
BugbearR <bugbearr@bugbearr.jp>
2020-09-20 18:47:47 +0900

    Convert to typescript

--- babel.config.js
+++ babel.config.js
@@ -19,7 +19,8 @@ module.exports = {
                 [
                     "minify"
                 ]
-            ]
+            ],
+            comments: false
         }
     }
 };
--- src/RadioNodeList_setValue.ts
+++ src/RadioNodeList_setValue.ts
@@ -1,10 +1,10 @@
 /**
  * Set value to RadioNodeList 
- * @param {RadioNodeList} list 
- * @param value
+ * @param {*} list 
+ * @param {string} value
  * @see https://developer.mozilla.org/en-US/docs/Web/API/RadioNodeList/value
  */
-export default function RadioNodeList_setValue(list, value) {
+export default function RadioNodeList_setValue(list: any, value: string): void {
     for (var i = 0; i < list.length; i++) {
         list[i].checked = (list[i].value === value);
     }
